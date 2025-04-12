02ccee9d4a188f26eb03b2dcef67de32b6302852
Koo In Young <zerokoo@gu-in-yeong-ui-Macmini.local>
2025-04-12 14:02:16 +0900

    feat: Upgrade tailwindcss to the latest

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,50 @@
 export default function Home() {
-  return <main className="p-24 text-lg font-bold">HomePage</main>;
+  return (
+    <main className="bg-base-300 h-full p-24 text-lg font-bold">
+      <h1 className="text-content">Content</h1>
+      <h1 className="text-content-light">Content Light</h1>
+      <h1 className="text-content-dark">Content Dark</h1>
+      <h1 className="text-primary">Primary</h1>
+      <div className="bg-primary">
+        <h1 className="text-primary-content">Primary Content</h1>
+      </div>
+      <h1 className="text-primary-light">Primary Light</h1>
+      <div className="bg-primary-light">
+        <h1 className="text-primary-content">Primary Content</h1>
+      </div>
+      <h1 className="text-secondary">Secondary</h1>
+      <div className="bg-secondary">
+        <h1 className="text-secondary-content">Secondary</h1>
+      </div>
+      <h1 className="text-secondary-light">Secondary Light</h1>
+      <div className="bg-secondary-light">
+        <h1 className="text-secondary-content">Secondary Content</h1>
+      </div>
+      <h1 className="text-secondary-light">Secondary Light</h1>
+      <h1 className="text-neutral">Neutral</h1>
+      <div className="bg-neutral">
+        <h1 className="text-neutral-content">Neutral Content</h1>
+      </div>
+      <h1 className="text-accent">Accent</h1>
+      <div className="bg-accent">
+        <h1 className="text-accent-content">Accent Content</h1>
+      </div>
+      <h1 className="text-info">Info</h1>
+      <div className="bg-info">
+        <h1 className="text-info-content">Info Content</h1>
+      </div>
+      <h1 className="text-success">Success</h1>
+      <div className="bg-success">
+        <h1 className="text-success-content">Success Content</h1>
+      </div>
+      <h1 className="text-warning">Warning</h1>
+      <div className="bg-warning">
+        <h1 className="text-warning-content">Warning Content</h1>
+      </div>
+      <h1 className="text-error">Error</h1>
+      <div className="bg-error">
+        <h1 className="text-error-content">Error Content</h1>
+      </div>
+    </main>
+  );
 }
